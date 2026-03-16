81e2a5e7327fc375ccd5c5351645527636507fd0
Tmax-3098 <ishaan.3098@gmail.com>
2026-03-16 16:59:08 +0530

    embeddings and vector store

--- src/test/java/com/sketch/learn_spring_ai/service/AiServiceTest.java
+++ src/test/java/com/sketch/learn_spring_ai/service/AiServiceTest.java
@@ -1,8 +1,12 @@
 package com.sketch.learn_spring_ai.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 public class AiServiceTest {
@@ -15,5 +19,25 @@ public class AiServiceTest {
         String topic = "rearwing";
         String output = aiService.getJoke(topic);
         System.out.println(output);
+    }
+
+//    @Test
+//    public void testAiServiceGetEmbedding(){
+//        float[] embeddings = aiService.getEmbedding("You know What that is ? Simply Lovely");
+//        System.out.println(Arrays.toString(embeddings));
+//        System.out.println(embeddings.length);
+//    }
+
+//    @Test
+//    public void testAiServiceInjestData(){
+//        aiService.ingestDataToVectorStore();
+//    }
+
+    @Test
+    public void testSimilaritySearch(){
+        List<Document> result = aiService.similaritySearch("space movie");
+        for(var doc: result){
+            System.out.println(doc);
+        }
     }
 }
